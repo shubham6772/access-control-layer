055a911bca688b5f887a6b754b40f9def8aa71e1
Devil Guru <Shubhamverma6772@gmail.com>
2026-06-25 18:48:44 +0530

    feature/jwt-validation-logic-and-session-store
    Co-authored-by: Shubham Verma <shubhamv@izooto.com>

--- internal/services/authservice.go
+++ internal/services/authservice.go
@@ -3,6 +3,8 @@ package services
 import (
 	"access-control-layer/internal/utils/jwt"
 	"access-control-layer/internal/utils/redis"
+	"errors"
+	"log"
 )
 
 type authService struct {
@@ -11,7 +13,7 @@ type authService struct {
 }
 
 type AuthService interface {
-	Validate(token string) (bool, error)
+	Validate(token string) (*jwt.Claims, error)
 }
 
 func NewAuthService(jwtClient *jwt.JwtClient, redisClient *redis.Client) *authService {
@@ -21,6 +23,43 @@ func NewAuthService(jwtClient *jwt.JwtClient, redisClient *redis.Client) *authSe
 	}
 }
 
-func (auth *authService) Validate(token string) (bool, error) {
-	return false, nil
+func (auth *authService) Validate(token string) (*jwt.Claims, error) {
+
+	claims, err := auth.jwtClient.Validate(token)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("UserID:", claims.UserID)
+	log.Println("VideoID:", claims.VideoID)
+	log.Println("SessionID:", claims.SessionID)
+
+	key := "session:" + claims.SessionID
+
+	log.Println("Redis Key:", key)
+
+	count, err := auth.redisClient.Rdb.Exists(
+		auth.redisClient.Ctx,
+		key,
+	).Result()
+
+	val, err := auth.redisClient.Rdb.Get(
+		auth.redisClient.Ctx,
+		"session:sess123",
+	).Result()
+
+	log.Println("VAL:", val)
+	log.Println("ERR:", err)
+
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Exists Count:", count)
+
+	if count == 0 {
+		return nil, errors.New("session not found")
+	}
+
+	return claims, nil
 }
